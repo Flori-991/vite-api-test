63446e927b8ca5f2e4aa31ed494e3404982b5a53
Flori-991 <Florian.Hecek@red-gate.com>
2023-06-16 12:47:17 +0100

    Initial Push

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import axios from "axios"
+import './App.css'
+
+function App() {
+    const placeHolder = 'Look up meanings';
+    const [wordToSearch, setWordToSearch] = useState('');
+    const [apiResponse, setApiResponse] = useState(null);
+    const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+    const searchOnEnter = (event) => {
+        if (event.key === 'Enter'){
+            getDictApiResponse();
+        }
+    }
+
+    const getDictApiResponse = async () => {
+        try {
+            setApiResponse(await axios.get(apiUrl + wordToSearch));
+        }
+        catch (err){
+            alert('Error calling the API!')
+        }
+    }
+
+  return (
+      <div className="App">
+          <div className='dictHeader'>
+
+          </div>
+          <div className='searchBar'>
+              <input
+                  placeholder={placeHolder}
+                  onKeyDown={searchOnEnter}
+                  onChange={event => setWordToSearch(event.target.value)}
+                  id='searchBarInput'/>
+              <button
+                  onClick={() => getDictApiResponse()}
+                  id='searchButton'>
+                  Search
+              </button>
+          </div>
+          {apiResponse && (
+              <>
+                  <div className='results'>
+                      <p>{apiResponse.data[0].phonetic}</p>
+                      {apiResponse.data[0].meanings.map(meaning => (<p key={meaning.definitions[0].definition}>{meaning.definitions[0].definition}</p>))}
+                  </div>
+              </>
+          )}
+      </div>
+  )
+}
+
+export default App
