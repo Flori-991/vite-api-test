b91ef5b36426cdb653aa3085b1a685d6149e89cc
Flori-991 <Florian.Hecek@red-gate.com>
2023-06-20 14:47:15 +0100

    Start table conversion

--- src/reactComponents/DataResponse.tsx
+++ src/reactComponents/DataResponse.tsx
@@ -1,26 +1,45 @@
 import React from "react";
 import { ApiResponse } from "../zodSchemas/ApiResponseSchema.ts";
+// import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper } from "@mui/material"
 
 const DataResponse: React.FunctionComponent<{ apiResponse: ApiResponse }> = ({
   apiResponse,
 }) => (
-  <div className="results">
-    <div>
-      {apiResponse[0].phonetics.map((phonetic) => (
-        <p key={`${phonetic.text} ${phonetic.audio}`}>{phonetic.text}</p>
-      ))}
-    </div>
-    <hr />
-    <ul className="meanings">
-      {apiResponse[0].meanings.map((meaning) =>
-        meaning.definitions.map((definition) => (
-          <li className="definitionText" key={`${definition.definition}`}>
-            {definition.definition}
-          </li>
-        ))
-      )}
-    </ul>
-  </div>
+  <table className="results">
+    <tbody>
+      <tr className="phoneticsRow">
+        {apiResponse.map((apiResponse) => (
+          <>
+            <th>
+              {apiResponse.phonetics.map((phonetic) => (
+                <p key={`${phonetic.text} ${phonetic.audio}`}>
+                  {phonetic.text}
+                </p>
+              ))}
+            </th>
+          </>
+        ))}
+      </tr>
+      <tr className="meaningsRow">
+        {apiResponse.map((apiResponse) => (
+          <>
+            <td>
+              {apiResponse.meanings.map((meaning) =>
+                meaning.definitions.map((definition) => (
+                  <p
+                    className="definitionText"
+                    key={`${definition.definition}`}
+                  >
+                    {definition.definition}
+                  </p>
+                ))
+              )}
+            </td>
+          </>
+        ))}
+      </tr>
+    </tbody>
+  </table>
 );
 
 export { DataResponse };
